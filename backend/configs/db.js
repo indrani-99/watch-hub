93aae40fd04c52f87b8f2005b7ea3b6fc52341bf
Ricky Kumar <anitasah926@gmail.com>
2024-07-18 00:03:40 +0530

    project_day01

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,1 +1,12 @@
-// database configuration
+const mongoose=require('mongoose');
+
+const connectDatabase=async(url)=>{
+    try{
+        await mongoose.connect(url)
+        console.log("mongodb connected");
+    }catch(err){
+        console.log(err);
+    }
+
+}
+module.exports=connectDatabase
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
+require('dotenv').config();
 const express=require('express');
+const connectDatabase = require('./configs/db');
+const userRouter = require('./routers/userRouter');
+const cors = require('cors');
+// const movieRouter = require('./routers/movieRouter');
+const app=express();
+app.use(cors());
+
+app.use(express.json());
+app.use(userRouter);
+// app.use(movieRouter)
+
+const port=process.env.port;
+app.listen(port,async()=>{
+    try{
+       await connectDatabase(process.env.mongodb_url);
+        console.log(`server is running at port ${port}`);
+
+    }catch(err){
+        console.log(err);
+    }
+})
