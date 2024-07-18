0472277f6438fcc3b61502b0e2c76297dcf23035
Indrani-Paul <paul.indrani999@gmail.com>
2024-07-18 23:18:19 +0530

    roomcreation added

--- backend/controller/roomController.js
+++ backend/controller/roomController.js
@@ -1,1 +1,42 @@
 // roomController 
+const { RoomModel } = require("../model/room.model");
+
+// Create a room
+const createRoom = async (req, res) => {
+  try {
+    const { username, userid , roomname} = req.body;
+    const newRoom = new RoomModel({
+      hostid: userid,
+      roomname,
+      hostname:username,
+      members: []
+    });
+    await newRoom.save();
+    res
+      .status(201)
+      .json({ message: "Room created successfully", room: newRoom });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+module.exports={createRoom}
+
+// Join a room
+exports.joinRoom = async (req, res) => {
+  try {
+    const { roomid } = req.params;
+    const { username, userid } = req.body;
+
+    const room = await RoomModel.findOne({_id:roomid});
+    if (!room) {
+      return res.status(404).json({ message: "Room not found" });
+    }
+
+    
+
+    res.status(200).json({ message: "Joined room successfully", room });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
--- backend/models/room.model.js
+++ backend/models/room.model.js
@@ -1,8 +1,24 @@
-const mongoose = require("mongoose");
+const mongoose=require('mongoose');
 
-const RoomSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  participants: [{ type: String }],
-});
 
-module.exports = mongoose.model("Room", RoomSchema);
+const memberSchema=new mongoose.Schema({
+    username: { type: String },
+    joinedAt: { type: Date, default :Date.now},
+    leftAt:{type: Date},
+    userid:{type: String},
+    role: { type: String, enum: ['host', 'participant'], default: 'participant' },
+}, {_id:false})
+const roomSchema=mongoose.Schema({
+hostid:{type:String, required:true},
+hostname:{type:String, required:true},
+createdAt:{type:Date, default :Date.now},
+deletedAt: {type:Date, default:null},
+roomname:{type:String,unique:true,required:true},
+members: { type: [memberSchema]}
+}, {versionKey:false})
+
+
+const RoomModel=mongoose.model('Room', roomSchema)
+module.exports={
+    RoomModel
+}
