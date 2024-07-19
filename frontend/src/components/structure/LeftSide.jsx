21c6bbe4d8a805175e743f272679e08ef5424077
sanjukumari-tech <foodlazzez@gmail.com>
2024-07-19 15:23:51 +0530

    this is structure of application

---
+++ frontend/src/components/structure/LeftSide.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+const LeftSide = () => {
+  return (
+    <div  style={{width: '15%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      height: '100vh',
+      background: '#fafafa',
+      padding: '5px', }}>LeftNavBar</div>
+  )
+}
+
+export default LeftSide;
---
+++ frontend/src/components/structure/RightSide.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+const RightSide = () => {
+  return (
+    <div  style={{ width: '25%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      height: '100vh',
+      background: '#fafafa',
+      padding: '5px',}}>RightNavbar</div>
+  )
+}
+
+export default RightSide;
